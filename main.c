56ba8f98af7e6c7233f6e2c051711859cecb9c72
btabtab <bladethace@gmail.com>
2021-11-26 03:17:46 +0000

    updated debug stuff

--- main.c
+++ main.c
@@ -9,6 +9,11 @@ typedef struct PhysicsObject
 }
 PhysicsObject;
 
+Color getPhysObjIDColour(PhysicsObject* physobj)
+{
+	return (Color){255 - physobj->ID * 3, 255 - physobj->ID * 2, 255 - physobj->ID * 4, 255};
+}
+
 Vector3 addVectors(Vector3 a, Vector3 b)
 {
 	return (Vector3){a.x + b.x, a.y + b.y, a.z + b.z};
@@ -26,8 +31,32 @@ Vector3 invertVector(Vector3 a)
 
 void renderPhysObjPosition(PhysicsObject* physobj)
 {
-	DrawSphere(physobj->position, 1, (Color){255 - physobj->ID * 3, 255 - physobj->ID * 2, 255 - physobj->ID * 4, 255});
-	DrawLine3D(physobj->position, (Vector3){0, 0, 0}, BLACK);
+	DrawSphere(physobj->position, 1, getPhysObjIDColour(physobj));
+}
+
+void renderPhysObjLineFromZZZ(PhysicsObject* physobj)
+{
+	DrawLine3D(physobj->position, (Vector3){0, 0, 0}, getPhysObjIDColour(physobj));
+}
+
+void drawLineToPhysObjGroundMarker(PhysicsObject* physobj)
+{
+	DrawLine3D(physobj->position, (Vector3){physobj->position.x, 0, physobj->position.z}, getPhysObjIDColour(physobj));
+}
+
+void renderPhysObjGroundAlignedMarker(PhysicsObject* physobj)
+{
+	DrawPlane((Vector3){physobj->position.x, 0, physobj->position.z}, (Vector2){1.f, 1.f}, getPhysObjIDColour(physobj));
+}
+
+void renderPhysObjGroundAlignedMarkerToZZZ(PhysicsObject* physobj)
+{
+	DrawLine3D((Vector3){0, 0, 0}, (Vector3){physobj->position.x, 0, physobj->position.z}, getPhysObjIDColour(physobj));
+}
+
+void renderPhysObjDirection(PhysicsObject* physobj)
+{
+	DrawLine3D(physobj->position, addVectors(physobj->position, physobj->direction), getPhysObjIDColour(physobj));
 }
 
 PhysicsObject newPhysicsObject(Vector3 pos, Vector3 direction)
@@ -47,7 +76,6 @@ float subtractPercentageFromNumber(float number, float percent)
 }
 void updatePhysicsObjectWeight(PhysicsObject* physobj)
 {
-	
 	physobj->direction.y -= (physobj->weight) / 10;
 }
 void updatePhysicsObjectDrag(PhysicsObject* physobj, float percent_lost)
@@ -63,6 +91,12 @@ void updatePhysicsObjectPosition(PhysicsObject* physobj)
 }
 
 void bouncePhysicsObject(PhysicsObject* physobj, Vector3 direction)
+{
+	physobj->direction = multiplyVectors(physobj->direction, direction);
+}
+
+//decay here is the amount that that the object's "bounce energy" will diminish.
+void bouncePhysicsObjectWithDecay(PhysicsObject* physobj, Vector3 direction, float decay_amount)
 {
 	physobj->direction = multiplyVectors(physobj->direction, direction);
 }
@@ -95,18 +129,33 @@ int main()
 			ClearBackground(BLUE);
 
 			BeginMode3D(camera);
-			renderPhysObjPosition(&test_obj);
-			updatePhysicsObjectPosition(&test_obj);
-			updatePhysicsObjectWeight(&test_obj);
-
-			if(test_obj.position.y < 0.f)
 			{
-				bouncePhysicsObject(&test_obj, (Vector3){1, -1, 1});
+				#define BOUNCE_BOUNDARY 500.f
+				DrawPlane((Vector3){0, -0.25, 0}, (Vector2){BOUNCE_BOUNDARY * 2, BOUNCE_BOUNDARY * 2}, (Color){125, 75, 25, 125});
+				renderPhysObjPosition(&test_obj);
+				renderPhysObjLineFromZZZ(&test_obj);
+				renderPhysObjGroundAlignedMarker(&test_obj);
+				drawLineToPhysObjGroundMarker(&test_obj);
+				renderPhysObjGroundAlignedMarkerToZZZ(&test_obj);
+				renderPhysObjDirection(&test_obj);
+
+				updatePhysicsObjectPosition(&test_obj);
+				updatePhysicsObjectWeight(&test_obj);
+				if(test_obj.position.y < 0.f)
+				{
+					bouncePhysicsObject(&test_obj, (Vector3){1, -1, 1});
+				}
+				if(test_obj.position.x < -BOUNCE_BOUNDARY || test_obj.position.x > BOUNCE_BOUNDARY)
+				{
+					bouncePhysicsObject(&test_obj, (Vector3){-1, 1, 1});
+				}
+				if(test_obj.position.z < -BOUNCE_BOUNDARY || test_obj.position.z > BOUNCE_BOUNDARY)
+				{
+					bouncePhysicsObject(&test_obj, (Vector3){1, 1, -1});
+				}
+				// DrawGrid(1000, 1);
+				camera.target = test_obj.position;
 			}
-
-			DrawGrid(1000, 1);
-
-			camera.target = test_obj.position;
 			EndMode3D();
 
 			DrawFPS(10, 10);
